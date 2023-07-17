36532473a6387e8ff70c3d46aeb360a29918f5f7
2633931483 <2633931483@qq.com>
2023-07-17 14:44:48 +0800

    增加绘制电子签名 Form_Signature

---
+++ Demo/Form_Signature.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Demo
+{
+    partial class Form_Signature
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_Signature = new System.Windows.Forms.PictureBox();
+            this.btn_Savec = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Signature)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_Signature
+            // 
+            this.pic_Signature.BackColor = System.Drawing.Color.White;
+            this.pic_Signature.Location = new System.Drawing.Point(12, 12);
+            this.pic_Signature.Name = "pic_Signature";
+            this.pic_Signature.Size = new System.Drawing.Size(776, 369);
+            this.pic_Signature.TabIndex = 0;
+            this.pic_Signature.TabStop = false;
+            this.pic_Signature.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Signature_Paint);
+            this.pic_Signature.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Signature_MouseDown);
+            this.pic_Signature.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_Signature_MouseMove);
+            // 
+            // btn_Savec
+            // 
+            this.btn_Savec.Location = new System.Drawing.Point(167, 397);
+            this.btn_Savec.Name = "btn_Savec";
+            this.btn_Savec.Size = new System.Drawing.Size(75, 35);
+            this.btn_Savec.TabIndex = 1;
+            this.btn_Savec.Text = "保 存";
+            this.btn_Savec.UseVisualStyleBackColor = true;
+            this.btn_Savec.Click += new System.EventHandler(this.btn_Savec_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(569, 397);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 35);
+            this.btn_Clear.TabIndex = 2;
+            this.btn_Clear.Text = "清 除";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form_Signature
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Savec);
+            this.Controls.Add(this.pic_Signature);
+            this.Name = "Form_Signature";
+            this.Text = "绘制电子签名";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Signature)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_Signature;
+        private System.Windows.Forms.Button btn_Savec;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
---
+++ Demo/Form_Signature.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form_Signature : Form
+    {
+        #region 定义变量
+
+        private System.Drawing.Drawing2D.GraphicsPath mousePath = new System.Drawing.Drawing2D.GraphicsPath();
+        //画笔透明度
+        private int myAlpha = 100;
+        //画笔颜色对象
+        private Color myUserColor = new Color();
+        //画笔宽度
+        private int myPenWidth = 3;
+        //签名的图片对象
+        public Bitmap SavedBitmap;
+
+        #endregion 
+
+        public Form_Signature()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Savec_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SavedBitmap = new Bitmap(pic_Signature.Width, pic_Signature.Height);
+                pic_Signature.DrawToBitmap(SavedBitmap, new Rectangle(0, 0, pic_Signature.Width, pic_Signature.Height));
+
+                #region 保存为透明的png图片
+
+                Bitmap bmp = SavedBitmap;
+                BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+                int length = data.Stride * data.Height;
+                IntPtr ptr = data.Scan0;
+                byte[] buff = new byte[length];
+                Marshal.Copy(ptr, buff, 0, length);
+                for (int i = 3; i < length; i += 4)
+                {
+                    if (buff[i - 1] >= 230 && buff[i - 2] >= 230 && buff[i - 3] >= 230)
+                    {
+                        buff[i] = 0;
+                    }
+                }
+                Marshal.Copy(buff, 0, ptr, length);
+                bmp.UnlockBits(data);
+                bmp.Save(@"D:\Test\TestSaveImg.png", ImageFormat.Png);
+
+                MessageBox.Show("保存成功");
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("保存失败，" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 清除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            pic_Signature.CreateGraphics().Clear(Color.White);
+            mousePath.Reset();
+        }
+
+        private void pic_Signature_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                try
+                {
+                    mousePath.AddLine(e.X, e.Y, e.X, e.Y);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            pic_Signature.Invalidate();
+        }
+
+        private void pic_Signature_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                mousePath.StartFigure();
+            }
+        }
+
+        private void pic_Signature_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                myUserColor = System.Drawing.Color.Blue;
+                myAlpha = 255;
+                Pen CurrentPen = new Pen(Color.FromArgb(myAlpha, myUserColor), myPenWidth);
+                e.Graphics.DrawPath(CurrentPen, mousePath);
+            }
+            catch { }
+        }
+    }
+}
